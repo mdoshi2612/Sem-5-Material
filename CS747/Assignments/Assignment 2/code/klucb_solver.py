de68c0131e0d4d8ffc110539e71f9654291f32db
Manav Doshi <200100094@iitb.ac.in>
2022-09-06 23:56:12 +0530

    Changes to 747 code

--- CS747/Assignments/Assignment 2/code/klucb_solver.py
+++ CS747/Assignments/Assignment 2/code/klucb_solver.py
@@ -1,36 +1,26 @@
 import numpy as np
 import math
 
+
 def KL(p, q):
-	if (p == 1):
-		return math.log(1/q)
-	
-	elif(p == 0):
-		return math.log((1)/(1-q))
-	
-	return p*math.log(p/q) + (1-p)*math.log((1-p)/(1-q))
+    if (p == 1):
+        return math.log(1/q)
 
-def solver(mean, time, count, c):
-	THRESHOLD = 0.001
-	STEP = 0.01
-	q = mean + STEP
-	rhs = (math.log(time) + c*math.log(math.log(time)))/count
-	while(KL(mean, q) - rhs < 0 and q < 0.99):
-		print(KL(mean, q) - rhs)
-		q += STEP   
-	return q
+    elif (p == 0):
+        return math.log((1)/(1-q))
 
-def solve_q(rhs, p_a):
-	if p_a == 1:
-		return 1 
-	q = np.arange(p_a, 1, 0.01)
-	lhs = []
-	for el in q:
-		lhs.append(KL(p_a, el))
-	print(lhs)
-	lhs_array = np.array(lhs)
-	lhs_rhs = lhs_array - rhs
-	lhs_rhs[lhs_rhs <= 0] = np.inf
-	print(lhs_rhs)	
-	min_index = lhs_rhs.argmin()
-	return q[min_index]
+    return p*math.log(p/q) + (1-p)*math.log((1-p)/(1-q))
+
+
+def rhs(time, count, c=3):
+    return (math.log(time) + c*math.log(math.log(time)))/count
+
+
+def solver(p, rhs, STEP=0.01):
+    q = 1-1e-5
+    while (q >= p and (KL(p, q) - rhs > 0)):
+        q -= STEP
+    return q
+
+
+print(solver((0.5), rhs(1000, 900, 3)))
--- CS747/Assignments/Assignment 2/code/task1.py
+++ CS747/Assignments/Assignment 2/code/task1.py
@@ -8,12 +8,12 @@ from. Here are the method details:
     - __init__(self, num_arms, horizon): This method is called when the class
         is instantiated. Here, you can add any other member variables that you
         need in your algorithm.
-    
+
     - give_pull(self): This method is called when the algorithm needs to
         select an arm to pull. The method should return the index of the arm
         that it wants to pull (0-indexed).
-    
-    - get_reward(self, arm_index, reward): This method is called just after the 
+
+    - get_reward(self, arm_index, reward): This method is called just after the
         give_pull method. The method should update the algorithm's internal
         state based on the arm that was pulled and the reward that was received.
         (The value of arm_index is the same as the one returned by give_pull.)
@@ -26,18 +26,21 @@ import numpy as np
 import math
 # Hint: math.log is much faster than np.log for scalars
 
+
 class Algorithm:
     def __init__(self, num_arms, horizon):
         self.num_arms = num_arms
         self.horizon = horizon
-    
+
     def give_pull(self):
         raise NotImplementedError
-    
+
     def get_reward(self, arm_index, reward):
         raise NotImplementedError
 
 # Example implementation of Epsilon Greedy algorithm
+
+
 class Eps_Greedy(Algorithm):
     def __init__(self, num_arms, horizon):
         super().__init__(num_arms, horizon)
@@ -45,13 +48,13 @@ class Eps_Greedy(Algorithm):
         self.eps = 0.1
         self.counts = np.zeros(num_arms)
         self.values = np.zeros(num_arms)
-    
+
     def give_pull(self):
         if np.random.random() < self.eps:
             return np.random.randint(self.num_arms)
         else:
             return np.argmax(self.values)
-    
+
     def get_reward(self, arm_index, reward):
         self.counts[arm_index] += 1
         n = self.counts[arm_index]
@@ -62,27 +65,47 @@ class Eps_Greedy(Algorithm):
 
 # START EDITING HERE
 # You can use this space to define any helper functions that you need
+def KL(p, q):
+    if (p == 1):
+        return math.log(1/q)
+
+    elif (p == 0):
+        return math.log((1)/(1-q))
+
+    return p*math.log(p/q) + (1-p)*math.log((1-p)/(1-q))
+
+
+def rhs(time, count, c=3):
+    return (math.log(time) + c*math.log(math.log(time)))/count
+
+
+def solver(p, rhs, STEP=0.005):
+    q = 1-1e-5
+    while (q >= p and (KL(p, q) - rhs > 0)):
+        q -= STEP
+    return q
+
 
 # END EDITING HERE
 
-    
+
 class UCB(Algorithm):
     def __init__(self, num_arms, horizon):
         super().__init__(num_arms, horizon)
         # You can add any other variables you need here
         # START EDITING HERE
-        self.time = self.num_arms
-        self.counts = np.ones(self.num_arms)
-        self.values = np.zeros(self.num_arms)
-        self.ucb = np.zeros(self.num_arms)
+        self.time = num_arms
+        self.counts = np.ones(num_arms)
+        self.values = np.zeros(num_arms)
+        self.ucb = np.zeros(num_arms)
         # END EDITING HERE
-    
+
     def give_pull(self):
         # START EDITING HERE
-        
+
         return np.argmax(self.ucb)
         # END EDITING HERE
-    
+
     def get_reward(self, arm_index, reward):
         # START EDITING HERE
         self.counts[arm_index] += 1
@@ -90,29 +113,35 @@ class UCB(Algorithm):
         n = self.counts[arm_index]
         value = self.values[arm_index]
         self.values[arm_index] = ((n - 1) / n) * value + (1 / n) * reward
-        self.ucb = [self.values[i] + math.sqrt((2*math.log(self.time)/self.counts[i])) for i in range(self.num_arms)]
+        self.ucb = [self.values[i] + math.sqrt(
+            (2*math.log(self.time)/self.counts[i])) for i in range(self.num_arms)]
         # END EDITING HERE
+
 
 class KL_UCB(Algorithm):
     def __init__(self, num_arms, horizon):
         super().__init__(num_arms, horizon)
         # You can add any other variables you need here
         # START EDITING HERE
-        self.time = self.num_arms
-        self.counts = np.ones(self.num_arms)
-        self.values = np.zeros(self.num_arms)
-        self.ucb = np.zeros(self.num_arms)
+        self.time = 1
+        self.counts = np.ones(num_arms)
+        self.values = np.zeros(num_arms)
+        self.ucb = np.zeros(num_arms)
         self.C = 3
         # END EDITING HERE
-    
+
     def give_pull(self):
         # START EDITING HERE
-        return 0
+        return np.argmax(solver(self.values[i], rhs(self.time, self.counts[i])) for i in range(self.num_arms))
         # END EDITING HERE
-    
+
     def get_reward(self, arm_index, reward):
         # START EDITING HERE
-        pass
+        self.counts[arm_index] += 1
+        self.time += 1
+        n = self.counts[arm_index]
+        value = self.values[arm_index]
+        self.values[arm_index] = ((n - 1) / n) * value + (1 / n) * reward
         # END EDITING HERE
 
 
@@ -121,19 +150,20 @@ class Thompson_Sampling(Algorithm):
         super().__init__(num_arms, horizon)
         # You can add any other variables you need here
         # START EDITING HERE
-        self.success = np.zeros(self.num_arms)
-        self.failure = np.zeros(self.num_arms)
+        self.generator = np.random.default_rng()
+        self.success = np.zeros(num_arms)
+        self.failure = np.zeros(num_arms)
 
         # END EDITING HERE
-    
+
     def give_pull(self):
         # START EDITING HERE
-        return np.argmax([np.random.default_rng().beta(self.success[i]+1, self.failure[i]+1) for i in range(self.num_arms)])
+        return np.argmax([self.generator.beta(self.success[i]+1, self.failure[i]+1) for i in range(self.num_arms)])
         # END EDITING HERE
-    
+
     def get_reward(self, arm_index, reward):
         # START EDITING HERE
-        if(reward == 1):
+        if (reward == 1):
             self.success[arm_index] += 1
         else:
             self.failure[arm_index] += 1
